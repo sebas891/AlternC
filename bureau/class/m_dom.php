ef4762e44054b874e576e1c7b94de370b67b8669
Benjamin Sonntag <benjamin@alternc.org>
2011-06-07 10:08:07 +0000

    fixing the dns/mx bug. Should Fixes #1247

--- bureau/class/m_dom.php
+++ bureau/class/m_dom.php
@@ -1017,12 +1017,7 @@ class m_dom {
     }
     
     $db->query("UPDATE domaines SET gesdns='$dns', gesmx='$gesmx' WHERE domaine='$dom'");
-    if ($dns=="0" && $r["dns"]=="1") {
-      $db->query("UPDATE domaines set dns_action='DELETE' where domaine='$dom';");
-    }
-    if ($dns=="1" && $r["dns"]=="0") {
-      $db->query("UPDATE domaines set dns_action='UPDATE' where domaine='$dom';");
-    }
+    $db->query("UPDATE domaines set dns_action='UPDATE' where domaine='$dom';");
     
     return true;
   } // edit_domain
