a7bf3446e47eb5be6e71bfbc609dca95e7783567
Sébastien Grenier <sebas@koumbit.org>
2018-02-02 11:16:44 -0300

    modify email edition to support custom delivery (3col.) thanks GuillaumeFromage

--- bureau/admin/mail_edit.php
+++ bureau/admin/mail_edit.php
@@ -32,7 +32,7 @@ $fields = array (
 	"passconf" => array("post","string",""),
 	"quotamb" => array("post","integer",0),
 	"enabled" => array("post","boolean",true),
-	"islocal" => array("post","boolean",true),
+	"islocal" => array("post","integer",1),
 	"recipients" => array("post","string",""),
 );
 getFields($fields);
@@ -58,7 +58,6 @@ if (!$res=$mail->get_details($mail_id)) {
 <?php
 $c=$admin->listPasswordPolicies();
 $passwd_classcount = $c['pop']['classcount'];
-
 echo $msg->msg_html_all();
 ?>
 
@@ -70,20 +69,25 @@ echo $msg->msg_html_all();
 
 <input type="hidden" name="mail_id" value="<?php ehe($mail_id); ?>" />
 <input type="hidden" name="new_account" value="<?php echo isset($new_account)?$new_account:false;?>" />
+<?php if (($custom_delivery = variable_get("custom_delivery", "", "None by default")) != "") { $span = 3; } else { $span = 2; } ?>
 <table class="tedit">
-  <tr><th colspan="2"><b><?php __("Is this email enabled?"); ?></b></th></tr>
+  <tr><th colspan="<?php echo $span; ?>"><b><?php __("Is this email enabled?"); ?></b></th></tr>
 
   <tr><td style="width: 50%; text-align: justify"><?php __("You can enable or disable this email anytime. This will bounce any mail received on this address, but will not delete the stored email, or the redirections or password."); ?><br />
 </td>
-    <td>
+<td colspan="<?php echo $span-1; ?>">
       <p>
 	<input type="radio" name="enabled" id="enabled0" class="inc" value="0"<?php cbox($enabled==0); ?> /><label for="enabled0"><?php __("No (email disabled)"); ?></label>
 	<input type="radio" name="enabled" id="enabled1" class="inc" value="1"<?php cbox($enabled==1); ?> /><label for="enabled1"><?php __("Yes (email enabled)"); ?></label>
       </p>
   </td></tr>
-
-  <tr><th colspan="2"><b><?php __("Is it a POP/IMAP account?"); ?></b></th></tr>
-  <tr><td style="width: 50%; text-align: justify"><?php __("POP/IMAP accounts are receiving emails in the server. To read those emails, you can use a Webmail, or a mail client such as Thunderbird. If you don't use POP/IMAP, you can configure your email to be a redirection to other existing emails. The maximum size is in megabytes, use 0 to make it infinite."); ?><br />
+  <tr><th colspan="<?php echo $span; ?>"><b><?php __("Is it a:"); ?></b></th></tr>
+  <tr>    <td>
+	<input type="radio" name="islocal" id="islocal1" class="inc" value="1"<?php !isset($new_account)?cbox($islocal==1):cbox($islocal==0); ?> onclick="popon()" /><label for="islocal1"><?php __("POP/IMAP account"); ?></label> </td><td>
+	<input type="radio" name="islocal" id="islocal0" class="inc" value="0"<?php !isset($new_account)?cbox($islocal==0):""; ?> onclick="popoff();" /><label for="islocal0"><?php __("A redirection to other email address"); ?></label></td>
+	<?php if ($custom_delivery != "") { ?><td><input type="radio" name="islocal" id="islocal2" class="inc" value="2" onclick="popon();" <?php cbox($islocal==2) ?>/><label for="islocal2"><?php __("A custom delivery") ; echo " ($custom_delivery)"; ?></label></td> <?php } ?>
+  </tr><tr>
+      <td style="width: 50%; text-align: justify"><?php __("POP/IMAP accounts are receiving emails in the server. To read those emails, you can use a Webmail, or a mail client such as Thunderbird. If you don't use POP/IMAP, you can configure your email to be a redirection to other existing emails. The maximum size is in megabytes, use 0 to make it infinite."); ?><br />
 <p>&nbsp;</p>
 <?php if ($islocal) { ?>
 <p><?php printf(_('This mailbox is currently using %1$s / %2$s'),format_size($used),format_size($quotabytes)); ?></p>
@@ -91,13 +95,7 @@ echo $msg->msg_html_all();
 <?php if ($mailbox_action=="DELETE") { ?>
 <p class="alert alert-warning"><?php __("This mailbox is pending deletion. You can recover its mails by setting it to 'Yes' NOW!"); ?></p>
 <?php } ?>
-</td>
-    <td>
-      <p>
-	<input type="radio" name="islocal" id="islocal0" class="inc" value="0"<?php !isset($new_account)?cbox($islocal==0):""; ?> onclick="popoff()" /><label for="islocal0"><?php __("No"); ?></label>
-	<input type="radio" name="islocal" id="islocal1" class="inc" value="1"<?php !isset($new_account)?cbox($islocal==1):cbox($islocal==0); ?> onclick="popon();" /><label for="islocal1"><?php __("Yes"); ?></label>
-      </p>
-      <div id="poptbl">
+<div id="poptbl">
 	<table class="tedit" >
           <tr id='mail_edit_pass' style='display: none;'><td colspan='2'><a href='javascript:mail_edit_pass();'><?php __("Click here to edit the existing password");?></a></td></tr>
 	  <tr id='mail_edit_pass1'><td><label for="pass"><?php __("Enter a POP/IMAP password"); ?></label></td><td><input type="password" class="int" autocomplete="off" name="pass" id="pass" value="" size="20" maxlength="32" /><?php display_div_generate_password(DEFAULT_PASS_SIZE,"#pass","#passconf",$passwd_classcount); ?></td></tr>
@@ -105,20 +103,21 @@ echo $msg->msg_html_all();
 	  <tr><td><label for="quotamb"><?php __("Maximum allowed size of this Mailbox"); ?></label></td><td><input type="text" class="int intleft" style="text-align: right" name="quotamb" id="quotamb" value="<?php ehe($quotamb); ?>" size="7" maxlength="6" /><span class="int intright"><?php __("MB"); ?></span></td></tr>
 	</table>
       </div>
-  </td></tr>
+  </td>
+<td>
+<table class="tedit">
   <?php if ($islocal) { ?>
 <tr id="turnoff" style="display: none;"><td colspan="2" class="alert alert-warning"><?php __("WARNING: turning POP/IMAP off will DELETE the stored messages in this email address."); ?></td></tr>
 <?php } ?>
-  <tr><th colspan="2"><b><?php __("Is it a redirection to other email addresses?"); ?></b></th></tr>
 
-  <tr><td style="width: 50%; text-align: justify"><label for="recipients"><?php __("If you want to send emails received on this address to other addresses, even outside this server, enter those recipients here."); ?></label></td><td>(<?php __("one recipient per line"); ?>)<br /><textarea class="int" cols="32" rows="5" name="recipients" id="recipients"><?php echo $recipients; ?></textarea></td></tr>
+  <tr><td style="width: 50%; text-align: justify"><label for="recipients"><?php __("If you want to send emails received on this address to other addresses, even outside this server, enter those recipients here."); ?></label></td></tr><tr><td>(<?php __("one recipient per line"); ?>)<br /><textarea class="int" cols="32" rows="5" name="recipients" id="recipients"><?php echo $recipients; ?></textarea></td></tr> </table>
 <?php 
    
    $html=$hooks->invoke("hook_mail_edit_html",array($mail_id,$type));
 foreach($html as $h) echo $h; 
 
 ?>
-<tr class="trbtn"><td colspan="2">
+	<tr class="trbtn"><td colspan="<?php echo $span; ?>">
   <input type="submit" class="inb ok" name="submit" value="<?php __("Change this email address"); ?>" />
   <input type="button" class="inb cancel" name="cancel" value="<?php __("Cancel"); ?>" onclick="window.history.go(-1);"/>
 </td></tr>
